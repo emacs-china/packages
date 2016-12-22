51f086f08fd9643d2994a35c9a58ebe671ee0abf
Chunyang Xu <mail@xuchunyang.me>
2016-12-23 00:33:57 +0800

    Add some custom ruby code to reduce code repetition

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,5 +1,11 @@
 include Nanoc::Helpers::HTMLEscape
 
+require 'json'
+
 def count_json file
   JSON.parse(open(file).read).length
+end
+
+def pkg_name item
+  File.basename item.identifier.without_ext
 end
