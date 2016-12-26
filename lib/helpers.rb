954feba2a23900f3eea0311b81588af341aaed6a
Chunyang Xu <mail@xuchunyang.me>
2016-12-26 18:02:37 +0800

    [#14] Fix download URLs of packages

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,6 +5,34 @@ require 'json'
 
 $all_packages_json = JSON.parse(open("content/all.json").read)
 
+$gnu_packages_json          = JSON.parse(open("content/gnu.json").read)
+$org_packages_json          = JSON.parse(open("content/org.json").read)
+$melpa_packages_json        = JSON.parse(open("content/melpa.json").read)
+$melpa_stable_packages_json = JSON.parse(open("content/melpa-stable.json").read)
+$marmalade_packages_json    = JSON.parse(open("content/marmalade.json").read)
+$user42_packages_json       = JSON.parse(open("content/user42.json").read)
+$sunrise_commander_packages_json     = JSON.parse(open("content/sunrise-commander.json").read)
+
+def pkg_json(pkg_name, elpa_name)
+  json = case elpa_name
+         when 'gnu'
+           $gnu_packages_json
+         when 'melpa'
+           $melpa_packages_json
+         when 'melpa-stable'
+           $melpa_stable_packages_json
+         when 'marmalade'
+           $marmalade_packages_json
+         when 'org'
+           $org_packages_json
+         when 'user42'
+           $user42_packages_json
+         when 'sunrise-commander'
+           $sunrise_commander_packages_json
+         end
+  json[pkg_name]
+end
+
 def count_json file
   JSON.parse(open(file).read).length
 end
@@ -36,7 +64,10 @@ def pkg_vers json_str
   if json["vers"]
     name = pkg_name
     type = json["type"] == "single" ? "el" : "tar"
-    json["vers"].map{|k,v| version = v.join('.'); link_to(version, "https://elpa.emacs-china.org/#{k}/#{name}-#{version}.#{type}") + " (#{k})"}.join(", ")
+    json["vers"].map{|k,v|
+      type = pkg_json(name, k)['type'] == "single" ? "el" : "tar"
+      version = v.join('.')
+      link_to(version, "https://elpa.emacs-china.org/#{k}/#{name}-#{version}.#{type}") + " (#{k})"}.join(", ")
   else
     '(none)'
   end
